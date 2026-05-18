8b8e93f4ad029d6ccf78964145bad49c8c24f1fd
OdinM13 <markodinius@gmail.com>
2026-05-18 18:15:54 +0200

    Feat: Add Player module and classes

---
+++ src/player.js
@@ -0,0 +1,14 @@
+import { GameBoard } from './gameboard.js';
+
+export class RealPlayer {
+  constructor () {
+    this.gameBoard = new GameBoard;
+  }
+}
+
+export class ComputerPlayer {
+  constructor () {
+    this.gameBoard = new GameBoard;
+  }
+}
+
