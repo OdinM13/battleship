3c7d78a1f08a0f5b6625759da2ea10274a66c226
OdinM13 <markodinius@gmail.com>
2026-06-03 08:41:27 +0200

    Feat: Add 2 tests to index.test.js

--- src/index.test.js
+++ src/index.test.js
@@ -2,6 +2,19 @@ import { Game } from './index.js';
 
 test('PopulateBoard', () => {
   const newGame = new Game;
-  newGame.populateBoard(newGame.realPlayer);
-  console.log(newGame.realPlayer.gameBoard);
+  newGame.populateBoard(newGame.computerPlayer);
+  console.log(newGame.computerPlayer.gameBoard);
+
+  const flatGameboard = newGame.computerPlayer.gameBoard.board.flat();
+  const filteredArray = flatGameboard.filter((value => value !== null));
+  // All ship length added together is 17
+  expect(filteredArray.length).toBe(17);
+})
+
+test('Random Board Population', () => {
+  const newGame1 = new Game;
+  const newGame2 = new Game;
+  newGame1.populateBoard(newGame1.computerPlayer);
+  newGame2.populateBoard(newGame2.computerPlayer);
+  expect(JSON.stringify(newGame1.computerPlayer.gameBoard.board)).not.toBe(JSON.stringify(newGame2.computerPlayer.gameBoard.board));
 })
